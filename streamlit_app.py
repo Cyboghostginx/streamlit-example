90b5d525f2120608d1033bca70db01aff6fbd697
Adetayo Adebimpe <146610955+Cyboghostginx@users.noreply.github.com>
2023-11-11 18:20:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header("Chat with the Streamlit docs 💬 📚")
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
